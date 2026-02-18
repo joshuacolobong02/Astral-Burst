89b2f07b5aa5d9aae5436a08d4c1ee85da8ba31e
joshuacolobong02 <joshua.colobong@gmlegacyholdings@gmail.com>
2026-02-18 11:45:21 +0800

    Feat: power ups

--- script/enemy_laser.gd
+++ script/enemy_laser.gd
@@ -23,7 +23,10 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		body.die()
+		if body.has_method("take_hit"):
+			body.take_hit()
+		else:
+			body.die()
 		queue_free()
 
 func _on_area_entered(area):
